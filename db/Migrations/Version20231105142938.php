1e4264a091f76ff371de9457db3051d896b86472
JZ <jz@dev.local>
2023-11-05 19:11:05 +0100

    migrations update

---
+++ db/Migrations/Version20231105142938.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231105142938 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `device` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, serial_number VARCHAR(255) NOT NULL, device_type enum(\'mower\',\'correction station\'), created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE `device`');
+    }
+}
